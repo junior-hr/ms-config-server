17de775558d78c21375c0f7fe9bb29989d884cb8
Junior Huanca Reyes <jr.huanca.08@gmail.com>
2022-11-07 14:20:31 -0500

    update rp

--- src/main/java/com/nttdata/bootcamp/msconfigserver/MsConfigServerApplication.java
+++ src/main/java/com/nttdata/bootcamp/msconfigserver/MsConfigServerApplication.java
@@ -4,12 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+/**
+ * Class MsConfigServerApplication Main.
+ * ConfigServer microservice class MsConfigServerApplication.
+ */
 @SpringBootApplication
 @EnableConfigServer
 public class MsConfigServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MsConfigServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MsConfigServerApplication.class, args);
+    }
 
 }
